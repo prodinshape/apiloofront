7ad3c8cdcacffcf0fb859b6ef9a3fe35b8bdfdca
Sam <samuel.s.mamane@gmail.com>
2022-12-27 16:29:57 +0100

    First commit

---
+++ src/containers/Root/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Root = () => {
+  return (
+    <div>
+      Root
+      <Link to="/workspaces">Go to workspaces</Link>
+    </div>
+  );
+};
+
+export default Root;
---
+++ src/containers/Workspaces/index.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+const Workspaces = () => (
+  <div>
+    Workspaces<Link to="/workspaces/plop">workspace plop</Link>
+  </div>
+);
+
+export default Workspaces;
